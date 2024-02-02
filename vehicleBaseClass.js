4d82e5eab58f64ebb209676c9de9300348e87357
sara pena <sarap@Saras-MacBook-Air.local>
2024-02-02 12:10:34 -0800

    Assignment Complete

--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -11,15 +11,6 @@ class Vehicle {
         this.numberOfWheels = 0;
     }
 
-    start() {
-        if (this.fuel > 0) {
-            return this.started = true;
-            console.log("engine started...!!!");
-        } else {
-            return this.started = false;
-            console.log("engine cannot start...");
-        }
-    }
     accelerate() {
         if (this.started) {
             if (this.fuel > 0) {
@@ -52,25 +43,10 @@ class Vehicle {
         }
     }
     stop() {
+        console.log('engine off')
         this.started = false;
     }
 
-    drive() {
-        accelerate();
-    }
-    brake() {
-        decelerate();
-    }
-
-    autoPark()
-    {
-
-    }
-
-    autoDrive()
-    {
-      
-    }
 
     typeOfVehicle(wheels) {
         if (this.numberOfWheels == 8 && 8 == wheels) {
